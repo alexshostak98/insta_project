40a234471aa2d1cedb0a175c242990d575a50061
alexshostak98 <alexandr_shostak@mail.ru>
2022-03-23 03:32:18 +0300

    all followers/followings some user and followings posts

--- user_app/api/serializers/user.py
+++ user_app/api/serializers/user.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from ...models import Profile
-from publication_app.api.serializers.publications import PostSerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -19,10 +18,10 @@ class ProfileSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'post', 'profile', 'followings_count', 'followers_count', ]
+        fields = ['id', 'username', 'post', 'profile', 'followings_count', 'followers_count', ]
 
     # profile = ProfileSerializer(allow_null=False, read_only=True)
-    post = PostSerializer(many=True, allow_null=True, read_only=True)
+    # post = PostSerializer(many=True, allow_null=True, read_only=True)
 
     followings_count = serializers.SerializerMethodField()
     followers_count = serializers.SerializerMethodField()
--- user_app/api/views/user.py
+++ user_app/api/views/user.py
@@ -1,9 +1,13 @@
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin
 from rest_framework import filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from ..serializers.user import UserSerializer, ProfileSerializer, CreateUserSerializer
 from django.contrib.auth.models import User
 from ...models import Profile
+from publication_app.models import Post
+from publication_app.api.serializers.publications import PostSerializer
 
 
 class UsersViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin):
@@ -16,3 +20,18 @@ class UsersViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateMod
 
     def get_serializer_class(self):
         return self.actions_serializers.get(self.action, self.serializer_class)
+
+    @action(methods=['get', ], detail=True, serializer_class=PostSerializer)
+    def followings_posts(self, request, pk, *args, **kwargs):
+        posts = Post.objects.filter(user__followers__follower=pk).order_by('-create_date').all()
+        return Response(self.get_serializer(posts, many=True).data)
+
+    @action(methods=['get', ], detail=True, serializer_class=UserSerializer)
+    def followers(self, request, pk, *args, **kwargs):
+        followers = User.objects.filter(followings__following=pk).all()
+        return Response(self.get_serializer(followers, many=True).data)
+
+    @action(methods=['get', ], detail=True, serializer_class=UserSerializer)
+    def followings(self, request, pk, *args, **kwargs):
+        followings = User.objects.filter(followers__follower=pk).all()
+        return Response(self.get_serializer(followings, many=True).data)
