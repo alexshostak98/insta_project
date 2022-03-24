87702077e40295483adb0610e60536ef46b334a4
alexshostak98 <alexandr_shostak@mail.ru>
2022-03-24 22:08:42 +0300

    ban on liking your posts and comments

--- like_app/api/serializers/like.py
+++ like_app/api/serializers/like.py
@@ -20,9 +20,19 @@ class LikePostSerializer(LikeSerializer):
         exclude = ['comment']
         read_only_fields = ['user']
 
+    def validate_post(self, value):
+        if value in self.context.get('request').user.post.all():
+            raise serializers.ValidationError("You can't like your posts")
+        return value
+
 
 class LikeCommentSerializer(LikeSerializer):
     class Meta:
         model = Like
         exclude = ['post']
         read_only_fields = ['user']
+
+    def validate_comment(self, value):
+        if value in self.context.get('request').user.comment.all():
+            raise serializers.ValidationError("You can't like your comments")
+        return value
